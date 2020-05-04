da84ac276db906aebbb847fa8899f72d46b29afb
Wesley Webster <wesleywebster91@gmail.com>
2020-05-03 22:39:43 -0500

    Refactoring Notes
    Clarity so when I look back on this I know

--- src/components/BookShelves/BookShelves.js
+++ src/components/BookShelves/BookShelves.js
@@ -3,6 +3,7 @@ import Shelf from './Shelf/Shelf';
 import Search from '../Search/Search';
 import * as BooksAPI from '../../BooksAPI';
 
+// CATEGORIES
 const SHELF_NAMES = {
     currentlyReading: "Currently Reading",
     wantToRead: "Want to Read",
@@ -11,7 +12,6 @@ const SHELF_NAMES = {
 
 const BookShelves = (props) => {
 
-    // State as an object, key-value pairs for category.
     const [books, setBooks] = useState({
         currentlyReading: [],
         wantToRead: [],
@@ -19,7 +19,10 @@ const BookShelves = (props) => {
     });
 
 
-    // Grabbing the data from our Book API
+
+    /*################################# 
+        FETCH FROM THE Book API
+    ################################### */
     useEffect(() => {
         BooksAPI.getAll().then(results => {
             const obj = results.reduce((eccumulator, currentValue) => {
@@ -37,19 +40,21 @@ const BookShelves = (props) => {
     }, []
     )
 
-       // Logic for the Select Values in Shelf.js to move books via the select tag
-       const moveBook = (book, shelfName) => {
+    /*################################# 
+            MOVING BOOKS LOGIC    
+    ################################### */
+    const moveBook = (book, shelfName) => {
         // Add new book to new shelf
         console.log(shelfName, books)
         const shelf = books[shelfName].slice(0)
         shelf.push(book);
-        
+
         // Remove old book from old shelf
         const oldShelfName = book.shelf
         const oldShelf = books[book.shelf].filter((currentValue) => currentValue.id !== book.id);
-        
+
         book.shelf = shelfName;
-      
+
         setBooks((prevState) => {
             return {
                 ...prevState,
@@ -69,8 +74,8 @@ const BookShelves = (props) => {
             <div className="list-books-content">
                 <div>
                     {keys.map((shelfName) => (
-                        <Shelf 
-                            sortBooks={books[shelfName]} 
+                        <Shelf
+                            sortBooks={books[shelfName]}
                             title={SHELF_NAMES[shelfName]}
                             moveBook={moveBook}
                             key={shelfName}
