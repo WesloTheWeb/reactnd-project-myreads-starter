de24d041c0872b816c5cedfdc6949355887e0a16
Wesley <wesleyadwebster@gmail.com>
2020-05-17 02:07:21 -0500

    Search functionality
    adding in some logic

--- src/components/SearchBooks/SearchBooks.js
+++ src/components/SearchBooks/SearchBooks.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import {Search, search} from '../../BooksAPI';
 
 const SearchBooks = (props) => {
+
+    const [query, setQuery] = useState("");
+
+    const searchQuery = () => {
+        setQuery(query);
+    }
+
+    const handleSearch = async e => {
+        try {
+            const query = e.target.value;
+            searchQuery();
+            const results = await search(query)
+            console.log(results);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div className="search-books">
             <div className="search-books-bar">
                 <Link to="/">
                     <button className="close-search">Close</button>
                 </Link>
-                <div className="search-books-input-wrapper">
+                <div className="search-books-input-wrapper" value={query}>
                     {/*
               NOTES: The search from BooksAPI is limited to a particular set of search terms.
               You can find these search terms here:
@@ -17,7 +37,7 @@ const SearchBooks = (props) => {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-                    <input type="text" placeholder="Search by title or author" />
+                    <input type="text" placeholder="Search by title or author" onChange={handleSearch} />
 
                 </div>
             </div>
